5f933733e83f1c40fcd2e4116ab1aad36de4472a
Takehiko Akimoto <Takehiko.Akimoto2@ibm.com>
2024-09-03 20:13:39 +0900

    feat: 練習3-5

---
+++ Ex3-5/src/Switch.java
@@ -0,0 +1,24 @@
+public class Switch {
+    public static void main(String[] args) throws Exception {
+        System.out.print("[メニュー] 1: 検索 2: 登録 3: 削除 4: 変更 > ");
+        int selected = new java.util.Scanner(System.in).nextInt();
+        switch (selected) {
+            case 1 -> {
+                System.out.println("検索します");
+            }
+            case 2 -> {
+                System.out.println("登録します");
+            }
+            case 3 -> {
+                System.out.println("削除します");
+            }
+            case 4 -> {
+                System.out.println("変更します");
+            }
+            default -> {
+                System.out.println("入力が正しくありません");
+                System.out.print("[メニュー] 1: 検索 2: 登録 3: 削除 4: 変更 > ");
+            }
+        }
+    }
+}
