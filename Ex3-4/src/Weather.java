4292c2d8c7f884f387398e657976460dc982d160
Takehiko Akimoto <Takehiko.Akimoto2@ibm.com>
2024-09-03 19:57:43 +0900

    feat: 練習3-4

---
+++ Ex3-4/src/Weather.java
@@ -0,0 +1,12 @@
+public class Weather {
+    public static void main(String[] args) throws Exception {
+        boolean weather = false;
+        if (weather == true) {
+            System.out.println("洗濯をします");
+            System.out.println("散歩に行きます");
+        } else {
+            System.out.println("映画を見ます");
+            System.out.println("寝ます");
+        }
+    }
+}
