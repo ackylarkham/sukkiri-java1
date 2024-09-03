81d8994e87e8d0dc7a83d854bcd8b14492f6749d
Takehiko Akimoto <Takehiko.Akimoto2@ibm.com>
2024-09-03 10:34:49 +0900

    feat: 変数の練習問題を追加

---
+++ Variables/src/Area.java
@@ -0,0 +1,9 @@
+public class Area {
+    public static void main(String[] args) throws Exception {
+        int width = 3;
+        int length = 5;
+        int area;
+        area = width * length;
+        System.out.println("縦幅" + width + "横幅" + length + "の長方形の面積は" + area);
+    }
+}
