7fdbdeaf52e151e6bdcba37c9fa088476c64f9b7
Takehiko Akimoto <Takehiko.Akimoto2@ibm.com>
2024-09-04 14:32:57 +0900

    feat:練習4-3

---
+++ Ex4-3/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+// exceptionのテスト
+    public static void main(String[] args) throws Exception {
+        int[] counts = null;
+        float[] heights = {171.3F, 175.0F};
+        //System.out.println(counts[1]); //NullPointerException
+        //System.out.println(heights[2]); //ArrayIndexOutOfBoundsException
+    }
+}
