78f013525dc4460f9330d6600eca2b80f377444b
Takehiko Akimoto <Takehiko.Akimoto2@ibm.com>
2024-09-04 14:11:46 +0900

    feat: 練習4-2

---
+++ Ex4-2/src/Array.java
@@ -0,0 +1,8 @@
+public class Array {
+    public static void main(String[] args) throws Exception {
+        int moneyList[] = {121902,8302,55100};
+        for (int balance : moneyList){
+            System.out.println("残高" + balance);
+        }    
+    }
+}
