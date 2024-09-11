5964d8d0308e703be0fde3d1901678749a60167a
Takehiko Akimoto <Takehiko.Akimoto2@ibm.com>
2024-09-11 14:06:45 +0900

    feat: 練習16-3を追加

--- Ex16-2/src/App.java
+++ Ex16-2/src/App.java
@@ -13,5 +13,17 @@ public class App {
         for (Hero h : heros) {
             System.out.println("ヒーローの名前: " + h.getName());
         }
+        
+        //練習16-3
+        Map<Hero,Integer> killEnemy = new HashMap<Hero,Integer>();
+
+        killEnemy.put(h1,3);
+        killEnemy.put(h2,7);
+
+        for (Hero key : killEnemy.keySet()) {
+            int v = killEnemy.get(key);
+            System.out.println(key.getName() + "が倒した敵 = " + v);
+        }
+
     }
 }
